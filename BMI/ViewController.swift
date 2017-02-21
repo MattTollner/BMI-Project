515b110c88f7456ccaff3761344de365ee64d541
MattTollner <linuxplus@LinuxPluss-Mac.local>
2017-02-21 20:25:43 +0000

    Added basic bmi calculation when return on both text boxes

--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
+    
+    var weight : Double?
+    var height : Double?
+    //Computed property
+    var bmi : Double? {
+        get {
+            if(weight != nil) && (height != nil){
+                return weight! / (height! * height!)
+            } else {
+                return nil
+            }
+        }
+    }
+    
     @IBOutlet weak var bmiLabel: UILabel!
     @IBOutlet weak var heightTxtField: UITextField!
     @IBOutlet weak var weightTxtField: UITextField!
@@ -28,6 +42,45 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         textField.resignFirstResponder()        
         return true
+    }
+    
+    func updateUI(){
+        if let b = self.bmi{
+            //update UI with calculated bmi (b)
+            self.bmiLabel.text = String(format: "%4.1f",b)
+        }
+    }
+    //textrFiled refernece to which is being edited
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        //If nill it will fail and return
+        guard let txt  : String = textField.text else {
+            return
+        }
+        
+        //This point txt is unwrapped and string
+        func conv(numString : String) -> Double?{
+        
+            let result : Double? = NumberFormatter().number(from: numString)?.doubleValue
+            return result
+            
+        }
+        
+        
+        switch (textField) {
+            
+        case heightTxtField:
+            self.height = conv(numString: txt)
+            
+        case weightTxtField:
+            self.weight = conv(numString: txt)
+            
+        default:
+            print("Somethign bad happened")
+        } //End of switch
+        
+        updateUI()
+        
     }
 
 
